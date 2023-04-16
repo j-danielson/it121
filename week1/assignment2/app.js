f5190a0847777d8b185740d7b9d64caa5773da58
Josh Danielson <37751792+j-danielson@users.noreply.github.com>
2023-04-16 15:09:26 -0700

    completed assignments

---
+++ week1/assignment2/app.js
@@ -0,0 +1,5 @@
+const myName = "Joshua Danielson";
+const myAge = 31;
+const coder = true;
+const message = "Hello, my name is " + myName + ", I am " + myAge+" years old and I can code JavaScript: " + coder + "."; 
+console.log(message);
